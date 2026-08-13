49a52ab1ad0b75fc8ca2eab72bc76100ad10b067
ahmedriadx10 <infinitecoderx02@gmail.com>
2026-08-13 16:18:02 +0600

    job details and company details route made meaningfull

--- src/app/(public)/browse-jobs/page.jsx
+++ src/app/(public)/browse-jobs/page.jsx
@@ -1,4 +1,22 @@
 const BrowseJobsPage = () => {
+  
+  /**
+   * ---- Browse Jobs Main Task---- 
+   *  A search bar with keyword input.  
+   *  Filter sidebar with options for job type, location, salary range, and category.  
+   *  Job listing cards displaying: title, company, location, type, and salary range.  
+   * Clicking a job card navigates to the Job Details page
+   
+  
+  
+  Now i'm talking about these pages flow 
+  
+  
+  
+  
+  
+  */
+
   return (
     <div>
       <h2></h2>
