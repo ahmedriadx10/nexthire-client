fa99939eaba28ae1cde2e61af0c9605209bb1bdd
ahmedriadx10 <infinitecoderx02@gmail.com>
2026-09-07 00:10:48 +0600

    remove console from proxy.js filed

---
+++ src/proxy.js
@@ -0,0 +1,44 @@
+import { NextResponse } from "next/server";
+import { auth } from "./lib/auth";
+import { headers } from "next/headers";
+
+// This function can be marked `async` if using `await` inside
+export async function proxy(request) {
+  const requestPathname = request.nextUrl.pathname;
+  const userWithSession = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  const user = userWithSession?.user;
+
+  const forbiddenUrl = new URL("/forbidden", request.url);
+
+  if (!userWithSession) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  if (
+    requestPathname.startsWith("/dashboard/seeker") &&
+    user?.role !== "seeker"
+  ) {
+    return NextResponse.redirect(forbiddenUrl);
+  }
+
+  if (
+    requestPathname.startsWith("/dashboard/recruiter") &&
+    user?.role !== "recruiter"
+  ) {
+    return NextResponse.redirect(forbiddenUrl);
+  }
+
+  if (
+    requestPathname.startsWith("/dashboard/admin") &&
+    user?.role !== "admin"
+  ) {
+    return NextResponse.redirect(forbiddenUrl);
+  }
+}
+
+export const config = {
+  matcher: "/dashboard/:role*",
+};
